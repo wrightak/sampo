7c21be1f551485dcb253ee50fa0588c71a8d8119
Andrew Wright <wrightak@gmail.com>
2018-08-19 06:11:39 +0100

    Added generated spring project

---
+++ server/src/main/kotlin/com/wrightak/sampo/SampoApplication.kt
@@ -0,0 +1,11 @@
+package com.wrightak.sampo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampoApplication>(*args)
+}
